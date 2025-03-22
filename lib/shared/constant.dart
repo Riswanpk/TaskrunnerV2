4a2ea1b97c223cdb00ac9b202cf2d3d24ac1512b
Riswan Pk <109125266+Riswanpk@users.noreply.github.com>
2025-03-22 12:07:40 +0530

    Update constant.dart

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,6 +1,6 @@
 class Constants {
-  static String appId = "1:860390615871:web:a5c2e858cf4c03af38418a";
-  static String apiKey = "AIzaSyC7t7lRW5sACltRpdA6oHga3HWxMhpBW7Q";
-  static String messagingSenderId = "860390615871";
-  static String projectId = "taskrunnerv2-4c675";
+  static String appId = "YOUR_APP_ID";
+  static String apiKey = "YOUR_API_KEY";
+  static String messagingSenderId = "YOUR_MESSAGING_SENDER_ID";
+  static String projectId = "YOUR_PROJECT_ID";
 }
